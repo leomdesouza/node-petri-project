585ee78e958af93628d11ab34f0a55ae90d8a317
leomdesouza <leosouza@kiddolabs.com>
2016-10-06 22:44:53 -0300

    config heroku

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
 
     "database": "mongodb://leomdesouza:admin@127.0.0.1:27017/userstory",
-    "mongoLabDatabase": "mongodb://leomdesouza:123123@ds053216.mlab.com:53216/iotproject",
+    "mongoLabDatabase": "mongodb://leosouza:123123@ds053206.mlab.com:53206/heroku_gxxkr942",
     "port": process.env.PORT || 3000,
     "secretKey": "corinthians"
 
