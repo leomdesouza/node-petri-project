871ddb97c0fd9bacfcc00e62ab5927697ee4c63b
leomdesouza <leosouza@kiddolabs.com>
2016-10-07 15:30:54 -0300

    adjust data model

--- app/models/data.js
+++ app/models/data.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var DataSchema = require('./schemas/device.js');
+var DataSchema = require('./schemas/data.js');
 
 module.exports = mongoose.model('Data', DataSchema);
